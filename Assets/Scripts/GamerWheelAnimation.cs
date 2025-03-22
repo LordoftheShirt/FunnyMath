e4e7d66a35e3ed118e29de988e903c0e11f3b6cb
LordoftheShirt <simon@polarcap.org>
2025-03-22 01:09:30 +0100

    Funnys Maths Day 1
    Gamer Wheel Highlights now work with controller. Still need to implement the ability for more Gamer Wheels to allow for local co-op.

---
+++ Assets/Scripts/GamerWheelAnimation.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamerWheelAnimation : MonoBehaviour
+{
+    public SmallCubeController myPlayer;
+    private Image[] childAnimations;
+    private int childCounter = 0;
+    private int focusNumber = 0;
+
+    [SerializeField] private Color[] colors;
+    void Start()
+    {
+        childAnimations = new Image[10];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).name.Length == 1)
+            {
+                childAnimations[childCounter] = transform.GetChild(i).GetComponent<Image>();
+                childCounter++;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (myPlayer != null)
+        {
+            if(myPlayer.GetAngle() < 360 && myPlayer.GetAngle() > 324)
+            {
+                childAnimations[5].color = colors[1];
+                focusNumber = 5;
+            }
+            else if (myPlayer.GetAngle() < 324 && myPlayer.GetAngle() > 288)
+            {
+                childAnimations[6].color = colors[1];
+                focusNumber = 6;
+            }
+            else if (myPlayer.GetAngle() < 288 && myPlayer.GetAngle() > 252)
+            {
+                childAnimations[7].color = colors[1];
+                focusNumber = 7;
+            }
+            else if (myPlayer.GetAngle() < 252 && myPlayer.GetAngle() > 216)
+            {
+                childAnimations[8].color = colors[1];
+                focusNumber = 8;
+            }
+            else if (myPlayer.GetAngle() < 216 && myPlayer.GetAngle() > 180)
+            {
+                childAnimations[9].color = colors[1];
+                focusNumber = 9;
+            }
+            else if (myPlayer.GetAngle() < 180 && myPlayer.GetAngle() > 144)
+            {
+                childAnimations[0].color = colors[1];
+                focusNumber = 0;
+            }
+            else if (myPlayer.GetAngle() < 144 && myPlayer.GetAngle() > 108)
+            {
+                childAnimations[1].color = colors[1];
+                focusNumber = 1;
+            }
+            else if (myPlayer.GetAngle() < 108 && myPlayer.GetAngle() > 72)
+            {
+                childAnimations[2].color = colors[1];
+                focusNumber = 2;
+            }
+            else if (myPlayer.GetAngle() < 72 && myPlayer.GetAngle() > 36)
+            {
+                childAnimations[3].color = colors[1];
+                focusNumber = 3;
+            }
+            else if (myPlayer.GetAngle() < 36 && myPlayer.GetAngle() > 0)
+            {
+                childAnimations[4].color = colors[1];
+                focusNumber = 4;
+            }
+
+            for (int i = 0; i < childAnimations.Length; i++)
+            {
+                if (i != focusNumber)
+                {
+                    if (childAnimations[i].color != colors[0])
+                    {
+                        childAnimations[i].color = colors[0];
+                    }
+                }
+            }
+
+            if (myPlayer.GetJoyInput().magnitude == 0)
+            {
+                if (childAnimations[focusNumber].color != colors[0])
+                {
+                    childAnimations[focusNumber].color = colors[0];
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/NewAgeControllers/SmallCubeController.cs
+++ Assets/Scripts/NewAgeControllers/SmallCubeController.cs
@@ -10,42 +10,57 @@ public class SmallCubeController : MonoBehaviour
     [SerializeField] private float cubeSpeed = 100;
 
     private Vector3 triangleStart, outOfBoundsCheck, constrainedDistanceVector;
-    private Vector2 rightStickMovementInput, leftStickMovementInput;
-    private LineRenderer lineRenderer;
+    private Vector2 rightStickMovementInput, leftStickMovementInput, getInputAmount;
+    private GameObject myWheel;
 
     private void Awake()
     {
         transform.SetParent(GameObject.Find("Gamer Wheel Canvas").transform);
-        triangleStart = GameObject.FindWithTag("GamerWheel1").transform.position;
+        myWheel = GameObject.FindWithTag("GamerWheel1");
+        if(myWheel.activeSelf)
+        {
+            myWheel.SetActive(true);
+            triangleStart = myWheel.transform.position;
+            myWheel.GetComponent<GamerWheelAnimation>().myPlayer = GetComponent<SmallCubeController>();
+        }
         cube.position = transform.parent.position;
         print("SPAWNED PLAYER: " + gameObject.name);
     }
 
     private void Update()
     {
-        //lineRenderer.SetPosition(0, triangleStart);
 
         if (rightStickMovementInput.magnitude > 0)
         {
             triangle.position = new Vector2(triangleStart.x + rightStickMovementInput.x * constrainedDistanceLength, triangleStart.y + rightStickMovementInput.y * constrainedDistanceLength);
             triangle.up = rightStickMovementInput;
+            getInputAmount = rightStickMovementInput;
         }
         else
         {
             triangle.position = new Vector2(triangleStart.x + leftStickMovementInput.x * constrainedDistanceLength, triangleStart.y + leftStickMovementInput.y * constrainedDistanceLength);
             triangle.up = leftStickMovementInput;
+            getInputAmount = leftStickMovementInput;
         }
-
     }
 
     private void FixedUpdate()
     {
         cube.Translate(new Vector2(rightStickMovementInput.x, rightStickMovementInput.y) * cubeSpeed * Time.deltaTime);
-
-        //print(leftStickMovementInput);
     }
 
     public void OnMove(InputAction.CallbackContext ctx) => rightStickMovementInput = ctx.ReadValue<Vector2>();
 
     public void LookWithArrow(InputAction.CallbackContext ctx1) => leftStickMovementInput = ctx1.ReadValue<Vector2>();
+
+    public float GetAngle()
+    {
+        return triangle.localEulerAngles.z;
+    }
+
+    public Vector2 GetJoyInput()
+    {
+
+        return getInputAmount;
+    }
 }
